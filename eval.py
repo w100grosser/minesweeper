7f9c10e6b3b4358c0eb86e3b59a5f9d9e28e1dd5
w100grosser <adamatoom@gmail.com>
2023-11-14 17:50:04 -0800

    Update training code and remove unnecessary files

---
+++ eval.py
@@ -0,0 +1,172 @@
+from engine import Minesweeper
+import numpy as np
+import autograd.numpy as anp
+from scipy.signal import convolve2d
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
+import random
+import time
+import os
+import wandb
+
+
+def visualize_board(game, flag=np.zeros((10,10))):
+    """
+    Visualizes the Minesweeper board using provided images.
+
+    :param game: Minesweeper game object
+    :return: None
+    """
+    # Mapping each cell value to its corresponding image
+    image_dict = {
+        -1: 'mine.png',
+        -3: 'flag.png',
+        -4: 'covered.png'
+    }
+    
+    board = game.board.copy()
+    board[flag == 1] = -3
+
+    for i in range(9):  # 0-8 for the number of mines in the surrounding cells
+        image_dict[i] = f'{i}.png'
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    # Go through each cell and display its corresponding image
+    for i in range(board.shape[0]):
+        for j in range(board.shape[1]):
+            # If the cell is revealed
+            if game.revealed[i][j] == 1:
+                cell_value = board[i][j]
+            else:
+                if board[i][j] == -3:
+                    cell_value = -3
+                else:
+                    cell_value = -4  # Covered/Hidden
+            ax.imshow(mpimg.imread(f'images/{image_dict[cell_value]}'), extent=(j, j+1, board.shape[0]-i-1, board.shape[0]-i))
+
+    ax.set_xticks(range(board.shape[1]))
+    ax.set_yticks(range(board.shape[0]))
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.grid(which='both')
+    plt.savefig(f'minesweeper_board.png')
+    plt.close(fig)  # Close the plot to free up memory
+
+
+class CustomDataset(Dataset):
+    def __init__(self, data_root, device='cuda'):
+        self.data_root = data_root
+        self.device = device
+        # Assuming that the files are named 'input_xxxx.npy' and 'target_xxxx.npy'
+        self.input_files = sorted([file for file in os.listdir(data_root) if file.startswith('input_')])
+        self.target_files = sorted([file for file in os.listdir(data_root) if file.startswith('target_')])
+
+    def __len__(self):
+        # Ensure the length is the same for both inputs and targets
+        return min(len(self.input_files), len(self.target_files))
+    
+    def __getitem__(self, idx):
+        # Construct full file paths
+        input_filepath = os.path.join(self.data_root, self.input_files[idx])
+        target_filepath = os.path.join(self.data_root, self.target_files[idx])
+        
+        # Load the arrays from the .npy files
+        input_array = np.load(input_filepath)
+        target_array = np.load(target_filepath)
+        
+        # Convert arrays to tensors
+        input_tensor = torch.from_numpy(input_array).float().to(self.device)
+        target_tensor = torch.from_numpy(target_array).float()
+        
+        return input_tensor, target_tensor
+    
+
+
+# Activation function
+def sigmoid(x):
+    return 1 / (1 + anp.exp(-x))
+
+def binary_crossentropy(predictions, targets):
+    epsilon = 1e-15  # To prevent log(0)
+    predictions = anp.clip(predictions, epsilon, 1 - epsilon)
+    loss = -anp.mean(targets * anp.log(predictions) + (1 - targets) * anp.log(1 - predictions))
+    return loss
+
+# Neural Network forward pass
+def neural_network(X, W1, b1, W2, b2):
+    print(X.shape)
+    X = X.flatten()
+    print(X.shape)
+    z1 = anp.dot(X, W1) + b1
+    a1 = sigmoid(z1)
+    z2 = anp.dot(a1, W2) + b2
+    return sigmoid(z2)
+    
+def gen_input_output(shape, num_mines, num_reveals = 0):
+
+    engine = Minesweeper(shape, num_mines)
+
+    i, j = engine.reveal_random()
+    engine.reveal_random_more(num_reveals)
+
+    def get_border(engine):
+        kernel = np.ones((3, 3))
+        convolved = convolve2d(engine.revealed, kernel, mode='same')
+        result = np.logical_and(convolved > 0, engine.revealed == 0).astype(int)
+        return result
+
+    border_mines = np.logical_and(get_border(engine), engine.board == -1).astype(int)
+
+    target_array = np.where(border_mines == 1, 1, 0)
+
+    input_array = np.zeros((2,*shape))
+
+    input_array[0] = 1 - engine.revealed
+
+    revealed_numbers = np.logical_and(engine.revealed, engine.board >= 0).astype(int)
+
+    input_array[1] = np.where(revealed_numbers, engine.board/8, 0)
+
+    return input_array, target_array, engine
+    
+if __name__ == '__main__':
+    
+    W1 = np.load('W1.npy')
+    b1 = np.load('b1.npy')
+    W2 = np.load('W2.npy')
+    b2 = np.load('b2.npy')
+
+    input_array, target_array, game = gen_input_output((10,10), 10, num_reveals = 2)
+    
+    
+    # Forward pass
+    pred = neural_network(input_array.flatten(), W1, b1, W2, b2)
+    pred = (pred >= 0.5).astype(int)
+    # reshape pred to match input_array
+    pred = pred.reshape(target_array.shape)
+    # apply sigmoid to pred
+    print(pred.shape)
+    print(pred)
+    print(target_array)
+    # quit()
+    
+    # plot these three side by side
+    visualize_board(game)
+    fig, ax = plt.subplots(nrows = 1, ncols = 3, figsize=(20, 10))
+    ax[0].imshow(plt.imread('minesweeper_board.png'))
+    ax[0].set_title("Original Game")    
+    visualize_board(game, flag=pred)
+    ax[1].imshow(plt.imread('minesweeper_board.png'))
+    ax[1].set_title("Predicton")    
+
+    
+
+    visualize_board(game, flag=target_array)
+    ax[2].imshow(plt.imread('minesweeper_board.png'))
+    ax[2].set_title("Ground Truth")    
+    
+    plt.show()
--- train.py
+++ train.py
@@ -4,6 +4,8 @@ from autograd import grad
 from data_gen import gen_input_output
 import random
 import os
+from tqdm import tqdm
+import glob
 
 base_dir = 'saved_models'
 
@@ -14,17 +16,32 @@ if not os.path.exists(base_dir):
 # Neural Network parameters
 shape = (10, 10)
 input_dim = 2 * shape[0] * shape[1]
-hidden_dim = 128
+hidden_dim = 2048
 output_dim = shape[0] * shape[1]
-learning_rate = 0.001
-epochs = 1000000
+learning_rate = 10
 num_mines = 15
 
+input_files = sorted(glob.glob(os.path.join("dataset", 'input_*.npy')))
+target_files = sorted(glob.glob(os.path.join("dataset", 'target_*.npy')))
+
+assert len(input_files) == len(target_files)
+
 # Initialize weights
-W1 = np.random.randn(input_dim, hidden_dim)
+# W1 = np.random.randn(input_dim, hidden_dim)
+# W1 = np.random.randn(input_dim, hidden_dim) * np.sqrt(2 / (input_dim + hidden_dim))
+W1 = np.random.randn(input_dim, hidden_dim) * np.sqrt(2 / input_dim)
 b1 = np.zeros((1, hidden_dim))
-W2 = np.random.randn(hidden_dim, output_dim)
+# W2 = np.random.randn(hidden_dim, output_dim)
+# W2 = np.random.randn(hidden_dim, output_dim) * np.sqrt(2 / (hidden_dim + output_dim))
+W2 = np.random.randn(hidden_dim, output_dim) * np.sqrt(2 / hidden_dim)
 b2 = np.zeros((1, output_dim))
+
+# load weights if they exist
+# if os.path.exists('W1.npy'):
+#     W1 = np.load('W1.npy')
+#     b1 = np.load('b1.npy')
+#     W2 = np.load('W2.npy')
+#     b2 = np.load('b2.npy')
 
 # Activation function
 def sigmoid(x):
@@ -64,24 +81,45 @@ grad_b1 = grad(loss, 1)
 grad_W2 = grad(loss, 2)
 grad_b2 = grad(loss, 3)
 
+
+loss_total = 0
+
+batch_size = 32
+epochs = 10
+
 # Training loop
 for epoch in range(epochs):
-    input_data, output_data = gen_input_output(shape, num_mines, num_reveals = random.randint(1, 5))
+    for batch in tqdm(range(len(input_files)//batch_size), desc=f"Epoch {epoch}", total=len(input_files)//batch_size):
+        dW1 = 0
+        db1 = 0
+        dW2 = 0
+        db2 = 0
+        batch_loss = 0
+        for input_file, target_file in zip(input_files[batch*batch_size: (batch + 1)*batch_size ], target_files[batch*batch_size: (batch + 1)*batch_size ]):
+            input_data, output_data = np.load(input_file), np.load(target_file)
 
-    dW1 = grad_W1(W1, b1, W2, b2, input_data, output_data)
-    db1 = grad_b1(W1, b1, W2, b2, input_data, output_data)
-    dW2 = grad_W2(W1, b1, W2, b2, input_data, output_data)
-    db2 = grad_b2(W1, b1, W2, b2, input_data, output_data)
-    
-    W1 -= learning_rate * dW1
-    b1 -= learning_rate * db1
-    W2 -= learning_rate * dW2
-    b2 -= learning_rate * db2
-    
-    if epoch % 50 == 0:
-        print(f"Epoch {epoch}, Loss: {loss(W1, b1, W2, b2, input_data, output_data)}")
+            dW1 += grad_W1(W1, b1, W2, b2, input_data, output_data)
+            db1 += grad_b1(W1, b1, W2, b2, input_data, output_data)
+            dW2 += grad_W2(W1, b1, W2, b2, input_data, output_data)
+            db2 += grad_b2(W1, b1, W2, b2, input_data, output_data)
 
-    if (epoch + 1) % 20000 == 0:
+            batch_loss += loss(W1, b1, W2, b2, input_data, output_data)
+        
+        loss_total += batch_loss/batch_size
+        W1 -= learning_rate * dW1 / batch_size
+        b1 -= learning_rate * db1 / batch_size
+        W2 -= learning_rate * dW2 / batch_size
+        b2 -= learning_rate * db2 / batch_size
+        # calculate running average loss
+        if batch == 0:
+            running_loss = batch_loss/batch_size
+        else:
+            running_loss = loss_total / batch
+        
+        if (batch) % 50 == 0:
+            tqdm.write(f"Epoch {epoch}, Batch: {batch}, Loss: {running_loss}")
+
+    if (epoch) % 2 == 0:
         # Create a directory for this epoch
         epoch_dir = os.path.join(base_dir, f'epoch_{epoch+1}')
         os.makedirs(epoch_dir)
@@ -91,7 +129,7 @@ for epoch in range(epochs):
         np.save(os.path.join(epoch_dir, 'b1.npy'), b1)
         np.save(os.path.join(epoch_dir, 'W2.npy'), W2)
         np.save(os.path.join(epoch_dir, 'b2.npy'), b2)
-        print(f"Saved model weights at epoch {epoch+1} in {epoch_dir}")
+        tqdm.write(f"Saved model weights at epoch {epoch+1} in {epoch_dir}")
 
 # Save the weights after training
 np.save('W1.npy', W1)
